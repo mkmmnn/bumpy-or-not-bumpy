77642650975f328d4f7280f97b195da5aeded187
mkmmnn <kammannmolly@gmail.com>
2024-12-15 15:45:36 -0500

    create basic iOS app files

---
+++ bumpy/bumpyApp.swift
@@ -0,0 +1,17 @@
+//
+//  bumpyApp.swift
+//  bumpy
+//
+//  Created by Molly Kammann on 12/15/24.
+//
+
+import SwiftUI
+
+@main
+struct bumpyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
